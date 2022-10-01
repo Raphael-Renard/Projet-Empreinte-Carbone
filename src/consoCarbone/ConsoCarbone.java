b848877a28ad74bccb57dcfa1666679d8458d510
angele5 <angele.ramauge@dauphine.eu>
2022-10-01 15:06:32 +0200

    Update ConsoCarbone.java

--- src/consoCarbone/ConsoCarbone.java
+++ src/consoCarbone/ConsoCarbone.java
@@ -2,6 +2,14 @@ package consoCarbone;
 
 public class ConsoCarbone {
 	private int id;
-	
+	static int nbConsoC;
+	//quels attributs ?
+	public ConsoCarbone(){
+        this(0);
+    }
+    public ConsoCarbone(int i){ 
+    	this.nbConsoC = this.nbConsoC +1;
+        this.id = this.nbConsoC;
+    }
 
 }
