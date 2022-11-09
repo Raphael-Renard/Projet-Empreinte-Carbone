71f3b8da23e462f1c36497ca1c17403344d0b2ae
angele5 <angele.ramauge@dauphine.eu>
2022-11-09 17:05:05 +0100

    Update Main.java

--- src/consoCarbone/Main.java
+++ src/consoCarbone/Main.java
@@ -1,27 +1,8 @@
 package consoCarbone;
 
-public class Main {
+public class Main { //classe contenant une méthode main permetant de faire des tests et de lancer le programme
 
 	public static void main(String[] args) {
-		Logement log = new Logement(75, CE.C);
-		// log.CalculImpactLog();
-		//Logement.CarbonMoyLog(log);
-		System.out.println(log);
-	
-		Alimentation a = new Alimentation(0.3,0.6);
-		//Alimentation.CarbonMoyAli(a);
-		// a.CalculImpactAli();
-		System.out.println(a);
-		
-		BienConso c = new BienConso(1200);
-		//BienConso.CarbonMoyBien(c);
-    	// c.CalculImpactDep();
-    	System.out.println(c);
-		
-		Transport transp = new Transport(true, Taille.G, 100000,340, 30000);
-		// transp.CalculImpactTransp(250);
-		 //Transport.CarbonMoyTransp(transp, 250);
-		System.out.println(transp);
 		
 		System.out.println("\n\nTest d'utilisation du polymorphisme avec toString : \n\n");
 		
