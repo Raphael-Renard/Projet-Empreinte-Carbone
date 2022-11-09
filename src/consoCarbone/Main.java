a5731675a515cddddeb40414146114e0de0bc643
Raphaël Renard <raphaelrenard2004@gmail.com>
2022-11-09 16:35:55 +0100

    remise en page des tests de polymorphisme

--- src/consoCarbone/Main.java
+++ src/consoCarbone/Main.java
@@ -3,36 +3,23 @@ package consoCarbone;
 public class Main {
 
 	public static void main(String[] args) {
-		Logement log = new Logement(75, CE.C);
-		// log.CalculImpactLog();
-		System.out.println(log);
-		
-		Alimentation a = new Alimentation(0.3,0.6);
-		//CarbonMoyAli();
-		// a.CalculImpactAli();
-		System.out.println(a);
-		
-		Transport transp = new Transport(true, Taille.G, 100000,340, 30000);
-		// transp.CalculImpactTransp(250);
-		// CarbonMoyTransp(transp, 250);
-		System.out.println(transp);
-		
+		// tests de polymorphisme
 		
 		// fait appel a la fonction toString de BienConso
     	ConsoCarbone c = new BienConso(20);
-    	System.out.println(c);
+    	System.out.println("1er cas :\n"+c+"\n");
 
 		// fait appel a la fonction toString de ConsoCarbone
 		ConsoCarbone d = new ConsoCarbone();
-    	System.out.println(d);
+    	System.out.println("2e cas :\n"+d+"\n");
 
 		// fait appel a la fonction toString de Logement
 		Logement log1 = new Logement();
-    	System.out.println(log1);
+    	System.out.println("3e cas :\n"+log1+"\n");
 		
-    	//fait appel a la fonction toString 
+    	//fait appel a la fonction toString de Logement 
     	ConsoCarbone log2 = new Logement(100,CE.A);
-    	System.out.println(log2);
+    	System.out.println("4e cas :\n"+log2+"\n");
 	}
 
 }
