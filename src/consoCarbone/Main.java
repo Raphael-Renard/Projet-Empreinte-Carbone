1b636e7aa97af43d88d9974ba8340a4a1c7c1849
Raphaël Renard <raphaelrenard2004@gmail.com>
2022-11-09 16:46:10 +0100

    retirer des bouts inutiles

--- src/consoCarbone/Main.java
+++ src/consoCarbone/Main.java
@@ -3,26 +3,25 @@ package consoCarbone;
 public class Main {
 
 	public static void main(String[] args) {
+
+		// tests des constructeurs et methodes
+
 		Logement log = new Logement(75, CE.C);
-		// log.CalculImpactLog();
 		//Logement.CarbonMoyLog(log);
 		System.out.println(log);
 	
 		Alimentation a = new Alimentation(0.3,0.6);
 		//Alimentation.CarbonMoyAli(a);
-		// a.CalculImpactAli();
 		System.out.println(a);
 		
 		BienConso c = new BienConso(1200);
 		//BienConso.CarbonMoyBien(c);
-    	// c.CalculImpactDep();
     	System.out.println(c);
 		
 		Transport transp = new Transport(true, Taille.G, 100000,340, 30000);
-		// transp.CalculImpactTransp(250);
-		 //Transport.CarbonMoyTransp(transp, 250);
+		//Transport.CarbonMoyTransp(transp, 250);
 		System.out.println(transp);
-		
+
 
 		// tests de polymorphisme
 		
