ebf8ec151e5232a88d724fba377a9f82d6e08a01
angele5 <angele.ramauge@dauphine.eu>
2022-11-09 16:26:33 +0100

    Creation méthode ConsoMoyBien

--- src/consoCarbone/BienConso.java
+++ src/consoCarbone/BienConso.java
@@ -30,14 +30,14 @@ public class BienConso extends ConsoCarbone{
 		this.montant=m;
 	}
     
+    public static void CarbonMoyBien(BienConso L){
+	    System.out.println("Votre empreinte carbone moyenne est de " + L.getImpact() + "T CO2 eq.");
+	    System.out.println("En moyenne les francais.e.s consomment 0.763 T CO2 eq à cause des vêtements, 0.682 à cause des autres biens et services et 1.180 à cause des achats et usages Internet et technologies.");
+    }
+    
     @Override
     public String toString() {
     	return ("id : "+super.getid()+",\nmontant : " + montant + " euros,\nimpact des depenses : " + this.getImpact()+"\n");
     }
 
-    public static void main(String[] args) {
-    	BienConso c = new BienConso(1200);
-    	// c.CalculImpactDep();
-    	System.out.println(c);
-     }
 }
--- src/consoCarbone/Main.java
+++ src/consoCarbone/Main.java
@@ -5,22 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		Logement log = new Logement(75, CE.C);
 		// log.CalculImpactLog();
+		//Logement.CarbonMoyLog(log);
 		System.out.println(log);
-		
+	
 		Alimentation a = new Alimentation(0.3,0.6);
-		//CarbonMoyAli();
+		//Alimentation.CarbonMoyAli(a);
 		// a.CalculImpactAli();
 		System.out.println(a);
 		
+		BienConso c = new BienConso(1200);
+		//BienConso.CarbonMoyBien(c);
+    	// c.CalculImpactDep();
+    	System.out.println(c);
+		
 		Transport transp = new Transport(true, Taille.G, 100000,340, 30000);
 		// transp.CalculImpactTransp(250);
-		// CarbonMoyTransp(transp, 250);
+		 //Transport.CarbonMoyTransp(transp, 250);
 		System.out.println(transp);
 		
 		
 		// fait appel a la fonction toString de BienConso
-    	ConsoCarbone c = new BienConso(20);
-    	System.out.println(c);
+    	ConsoCarbone c1 = new BienConso(20);
+    	System.out.println(c1);
 
 		// fait appel a la fonction toString de ConsoCarbone
 		ConsoCarbone d = new ConsoCarbone();
@@ -33,6 +39,7 @@ public class Main {
     	//fait appel a la fonction toString 
     	ConsoCarbone log2 = new Logement(100,CE.A);
     	System.out.println(log2);
+    	
 	}
 
 }
