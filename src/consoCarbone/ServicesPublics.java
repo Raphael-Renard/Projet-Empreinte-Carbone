207dcbe47880633a29d125edbdb526e8dd8269d2
angele5 <angele.ramauge@dauphine.eu>
2022-11-19 15:29:29 +0100

    Ajout méthodes dans User

--- src/consoCarbone/ServicesPublics.java
+++ src/consoCarbone/ServicesPublics.java
@@ -3,6 +3,7 @@ package consoCarbone;
 public class ServicesPublics extends ConsoCarbone{
 	static final double empreinte=1.5;
 	//singleton
-	
-	
+	public ServicesPublics() {
+		super.setimpact(empreinte);
+	}
 }
--- src/users/User.java
+++ src/users/User.java
@@ -8,4 +8,22 @@ public class User {
 	Logement logement;
 	Transport transport;
 	ServicesPublics services;
+	
+	public User() {
+		//utiliser constructeurs des conso
+	}
+	
+	private double calculerEmpreinte() {
+		return (alimentation.getImpact()+bienConso.getImpact()+logement.getImpact()+transport.getImpact()+services.getImpact());
+	}
+	
+	private void detaillerEmpreinte() {
+		System.out.println("L'utilisateur consomme "+alimentation.getImpact()+" à cause de l'alimentation.");
+		System.out.println("L'utilisateur consomme "+bienConso.getImpact()+" à cause des dépenses.");
+		System.out.println("L'utilisateur consomme "+logement.getImpact()+" à cause du logement.");
+		System.out.println("L'utilisateur consomme "+transport.getImpact()+" à cause des transports.");
+		System.out.println("L'utilisateur consomme "+services.getImpact()+" à cause des services publiques.");
+		System.out.println("Au total la consommation de l'utilisateur est de "+this.calculerEmpreinte());
+
+	}
 }
