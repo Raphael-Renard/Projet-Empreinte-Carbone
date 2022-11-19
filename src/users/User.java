f6e6fa792b01d5bf06bc90035cb9afc8450944de
angele5 <angele.ramauge@dauphine.eu>
2022-11-19 15:16:35 +0100

    Création User

---
+++ src/users/User.java
@@ -0,0 +1,11 @@
+package users;
+
+import consoCarbone.*;
+
+public class User {
+	Alimentation alimentation;
+	BienConso bienConso;
+	Logement logement;
+	Transport transport;
+	ServicesPublics services;
+}
