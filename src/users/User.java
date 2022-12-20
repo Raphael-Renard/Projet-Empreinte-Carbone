a640d1e7022361dcf95259f36ec1cf9744aa158f
angele5 <angele.ramauge@dauphine.eu>
2022-12-20 21:30:44 +0100

    ajout liste logements / transports

--- src/users/User.java
+++ src/users/User.java
@@ -19,11 +19,11 @@ public class User {
 	
 	/** poste de consommation carbone lié au logement
 	*/
-	private Logement logement;
+	private ArrayList<Logement> listeLogement;
 	
 	/** poste de consommation carbone lié au transport
 	*/
-	private Transport transport;
+	private ArrayList<Transport> listeTransport;
 	
 	/** poste de consommation carbone lié aux services publics
 	*/
@@ -43,8 +43,10 @@ public class User {
 	public User() {
 		alimentation = new Alimentation();
 		bienConso = new BienConso();
-		logement = new Logement();
-		transport = new Transport();
+		listeLogement = new ArrayList<Logement>();
+		listeLogement.add(new Logement());
+		listeTransport = new ArrayList<Transport>();
+		listeTransport.add(new Transport());
 		services = new ServicesPublics();
 		habitudes = new HabitudesAli();
 		empreinte = calculerEmpreinte();
@@ -57,18 +59,28 @@ public class User {
 		alimentation = new Alimentation(txBoeuf, txVege);
 		habitudes = new HabitudesAli(saison, locaux, gaspillage, vrac, ecommerce);
 		bienConso = new BienConso(montant);
-		logement = new Logement(superficie, ce);
-		transport = new Transport(possede, taille, kilomAnnee, amortissement, fabrication);
+		listeLogement = new ArrayList<Logement>();
+		listeLogement.add( new Logement(superficie, ce));
+		listeTransport = new ArrayList<Transport>();
+		listeTransport.add(new Transport(possede, taille, kilomAnnee, amortissement, fabrication));
 		services = new ServicesPublics();
 		empreinte = calculerEmpreinte();
-	}
+	} //Pb : initialiser les listes
 	
 	/**
 	 * Méthode qui calcule l'empreinte carbone de l'utilisateur.rice
 	 * @return empreinte carbone
 	 */
 	private double calculerEmpreinte() {
-		return (alimentation.getImpact()+habitudes.getImpact()+bienConso.getImpact()+logement.getImpact()+transport.getImpact()+services.getImpact());
+		int sommelogement=0;
+		for (Logement loge : listeLogement) {
+			sommelogement += loge.getImpact();
+		}
+		int sommetransport=0;
+		for (Transport transp : listeTransport) {
+			sommetransport += transp.getImpact();
+		}
+		return (alimentation.getImpact()+habitudes.getImpact()+bienConso.getImpact()+sommelogement+sommetransport+services.getImpact());
 	}
 	
 	/**
@@ -78,8 +90,12 @@ public class User {
 		System.out.println("L'utilisateur consomme "+alimentation.getImpact()+" à cause de l'alimentation.");
 		System.out.println("L'utilisateur consomme "+habitudes.getImpact()+" à cause de vos habitudes alimentaires.");
 		System.out.println("L'utilisateur consomme "+bienConso.getImpact()+" à cause des dépenses.");
-		System.out.println("L'utilisateur consomme "+logement.getImpact()+" à cause du logement.");
-		System.out.println("L'utilisateur consomme "+transport.getImpact()+" à cause des transports.");
+		for (Logement loge : listeLogement) {
+			System.out.println("L'utilisateur consomme "+loge.getImpact()+" à cause du logement "+loge.getid()+".");
+		}
+		for (Transport transp : listeTransport) {
+			System.out.println("L'utilisateur consomme "+transp.getImpact()+" à cause des transports.");
+		}
 		System.out.println("L'utilisateur consomme "+services.getImpact()+" à cause des services publiques.");
 		System.out.println("Au total la consommation de l'utilisateur est de "+this.calculerEmpreinte());
 
@@ -94,19 +110,19 @@ public class User {
 		liste.add(alimentation);
 		liste.add(habitudes);
 		liste.add(bienConso);
-		liste.add(logement);
-		liste.add(transport);
+	//	liste.add(logement);    quelle solution pour comparer ??
+	//	liste.add(transport);
 		liste.add(services);
 		Collections.sort(liste);
 		for (ConsoCarbone poste_conso : liste) {
 			System.out.println(poste_conso.getClass().getSimpleName()+", impact : "+poste_conso.getImpact()+" TCO2eq\n");
 		}
 		System.out.println("Impact max :\n"+Collections.max(liste));
-		Collections.max(liste).conseil(); // pb : se réfère tjrs à la méthode de ConsoCarbone
+		Collections.max(liste).conseil();
 	}
 
 	public static void main(String[] args) {
 		User utilisateur = new User(0.4, 0.5, false, false, 259, true, true,1500, 200,CE.A, true, Taille.P, 1500, 10, 0.4);
 		utilisateur.ordonne();
 	}
-}
+} 
