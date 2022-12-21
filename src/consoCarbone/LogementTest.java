3b363e1242c309d1e5959179a5e38f3e8410ab3f
angele5 <angele.ramauge@dauphine.eu>
2022-12-21 20:58:41 +0100

    testJunits Logement

---
+++ src/consoCarbone/LogementTest.java
@@ -0,0 +1,34 @@
+package consoCarbone;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LogementTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testConseil() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetsuperficie() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetce() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
