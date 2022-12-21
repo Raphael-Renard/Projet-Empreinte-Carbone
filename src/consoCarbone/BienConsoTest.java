ca8a76c5acecf936c204d099338a574beec2d641
Raphaël Renard <raphaelrenard2004@gmail.com>
2022-12-22 00:11:12 +0100

    début de test JUnit

--- src/consoCarbone/BienConsoTest.java
+++ src/consoCarbone/BienConsoTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,9 +14,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class BienConsoTest {
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private PrintStream stdOut = System.out ;
+	
 	private BienConso bien;
+
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -38,13 +40,16 @@ class BienConsoTest {
 	}
 	
 	@ParameterizedTest
-	@CsvSource({ "3000,Essayez de faire moins d'achats si cela ne vous est pas utile. Essayer d'acheter des objets reconditionnés, des vêtements dans des friperies,...", "12,Essayez de faire moins d'achats si cela ne vous est pas nécéssaire."})
+	@CsvSource(value ={"3000-Essayez de faire moins d'achats si cela ne vous est pas utile. Essayer d'acheter des objets reconditionnés, des vêtements dans des friperies,...", "12-Essayez de faire moins d'achats si cela ne vous est pas nécéssaire."}, delimiter='-')
 	void testConseil(double m, String rep) {
 		// Act
+		ByteArrayOutputStream out = new ByteArrayOutputStream();    
+		System.setOut(new PrintStream(out));
 		this.bien.setmontant(m);
 		this.bien.conseil();
 		// Assert
-		assertEquals(rep,outContent.toString());
+		String s=out.toString();
+		assertEquals(rep,s.substring(0,s.lastIndexOf("\n")-1 ));
 		//fail("Not yet implemented"); // TODO
 	}
 
